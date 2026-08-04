14fe7ab770425e763129a195dffa28d8139f1da4
tangyoucheng <youchengtang@aliyun.com>
2026-08-04 15:17:17 +0900

    Update common_oracle_table_config.py

--- Oracle不同数据库表数据比较/common_oracle_table_config.py
+++ Oracle不同数据库表数据比较/common_oracle_table_config.py
@@ -6,13 +6,23 @@
 # 【重要】業務主キーを指定（単一、または複合主キーのリスト）
 # 例1（単一カラム）: BUSINESS_PKS = ["ORDER_NO"]
 # 例2（複合カラム）: BUSINESS_PKS = ["USER_ID", "PRODUCT_ID"]
+#
+# EXTRA_WHERE に比較したい特定の条件を SQL 形式で記述します。
+# 条件がない場合は、"EXTRA_WHERE": None または空文字 "" にしてください。
 TABLES_CONFIG = [
     {
         "TABLE_NAME": "TCMMSGINFO",
-        "BUSINESS_PKS": ["SQSEQ"]
+        "BUSINESS_PKS": ["SQSEQ"],
+        "EXTRA_WHERE": "CDSOSINMOTOCMP = '00' AND DTUPD >= TO_DATE('2022-01-01', 'YYYY-MM-DD')",
     },
     {
         "TABLE_NAME": "TCMMTHOJO",
-        "BUSINESS_PKS": ["CDCMP", "KBMST", "CDCODE"]
+        "BUSINESS_PKS": ["CDCMP", "KBMST", "CDCODE"],
+        "EXTRA_WHERE": "" ,
+    },
+    {
+        "TABLE_NAME": "TCMMTHANYO",
+        "BUSINESS_PKS": ["CDCMP", "KBMST", "CDCODE"],
+        "EXTRA_WHERE": "CDCMP = '00'" ,
     },
 ]
